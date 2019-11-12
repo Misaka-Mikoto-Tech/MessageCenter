62a2446528e54ae104071b4e274e5bbae2e580f9
easy66 <easy66@live.com>
2019-11-13 05:33:58 +0800

    + 首次提交

---
+++ Assets/Scripts/MsgDef.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MsgId
+{
+    MsgA,
+    MsgB,
+}
+
+public static class MsgTypeBinder
+{
+    public static void BindTypes(Dictionary<MsgId, Type> dicType)
+    {
+        dicType.Add(MsgId.MsgA, typeof(Action<int>));
+        dicType.Add(MsgId.MsgB, typeof(Action<float, string>));
+    }
+}
+
+
+
+
+        
+
+
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        MsgCenter.Register<int>(MsgId.MsgA, OnMsgA);
+        MsgCenter.Register<float, string>(MsgId.MsgB, OnMsgB);
+    }
+
+    void OnDestroy()
+    {
+        MsgCenter.UnRegister<int>(MsgId.MsgA, OnMsgA); // TODO 可以由 UIManager.CloseUI 自动执行
+        MsgCenter.UnRegister<float, string>(MsgId.MsgB, OnMsgB);
+    }
+
+    void OnEnable()
+    {
+        MsgCenter.SendMsg(MsgId.MsgA, 5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMsgA(int val)
+    {
+        Debug.LogFormat("OnMsgA:{0}", val);
+    }
+
+    void OnMsgB(float f, string str)
+    {
+        Debug.LogFormat("OnMsgB:{0}, {1}", f, str);
+    }
+}

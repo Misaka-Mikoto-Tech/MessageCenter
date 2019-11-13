bfdca74b98c0f416ca7f7e4b30cc9a62f612691f
easy66 <easy66@live.com>
2019-11-14 07:34:13 +0800

    * 给消息枚举定义添加注释，以方便 ide 显示

--- Assets/Scripts/MsgDef.cs
+++ Assets/Scripts/MsgDef.cs
@@ -3,9 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 消息类型定义
+/// </summary>
 public enum MsgId
 {
+    /// <summary>
+    /// 测试消息A, 参数 (int)
+    /// </summary>
     MsgA,
+    /// <summary>
+    /// 测试消息B, 参数 (float, string)
+    /// </summary>
     MsgB,
 }
 
